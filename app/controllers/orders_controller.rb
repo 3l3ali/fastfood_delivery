cd9aa24c477c2310b789ac114f34c521d96d7383
Mohammad Alali <m.a.alsyouf@gmail.com>
2017-07-11 16:43:30 +0900

    made orders controller with all its actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,47 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
   def index
+    @orders = current_user.orders
   end
 
   def show
   end
 
   def new
+    @order = Order.new
   end
 
   def create
+    @order = current_user.orders.new(order_params)
+
+    if @order.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @order = current_user.orders.find(params[:id])
   end
 
   def update
+    @order.update(order_params)
+    redirect_to root_path
   end
 
   def destroy
+    @order.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_order
+    @order = current_user.orders.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:notes, :status, :bill)
   end
 end
