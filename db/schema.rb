d38e6a367403b1fcef49e9a5c66c0e9af3ebef0b
Mohammad Alali <m.a.alsyouf@gmail.com>
2017-07-14 13:54:17 +0900

    fixed links for dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20170714033239) do
   create_table "items_orders", id: false, force: :cascade do |t|
     t.integer "order_id", null: false
     t.integer "item_id",  null: false
-    t.index ["item_id", "order_id"], name: "index_items_orders_on_item_id_and_order_id", using: :btree
     t.index ["order_id", "item_id"], name: "index_items_orders_on_order_id_and_item_id", using: :btree
   end
 
