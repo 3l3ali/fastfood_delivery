0a1e8789dc26d7a467f41188d60142d1f9fb1a6c
Mohammad Alali <m.a.alsyouf@gmail.com>
2017-07-14 15:24:57 +0900

    manager page orders done

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   post "/cart", to: "orders#add_cart"
   post "/remove_item", to: "orders#remove_cart"
   put "/clear_cart", to: "orders#clear_cart"
+
   get '/manager/orders', to: "orders#manager_orders"
+  patch '/manager/orders/prepare/:id', to: "orders#to_prepare", as: "prepare"
+  patch '/manager/orders/done/:id', to: "orders#to_done", as: "done"
+  patch '/manager/orders/out/:id', to: "orders#to_out", as: "out"
+  patch '/manager/orders/delivered/:id', to: "orders#to_delivered", as: "delivered"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, except: [:create, :update] do
